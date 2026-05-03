f2ab3a07d6e225f9830a77fd9d95c3d489847115
muhammad <css001823@must.ac.mw>
2026-05-03 17:27:09 +0200

    sessions on navigation and lending php files

--- desk-navigation.php
+++ desk-navigation.php
@@ -1,6 +1,7 @@
 <?php
-
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
         // Protect page (redirect if not logged in)
         if (!isset($_SESSION['user_id']) && $_SESSION['role'] != 'desk') {
             header("Location: login.php");
