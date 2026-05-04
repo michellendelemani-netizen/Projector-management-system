7506a1a4cc92fdc044314da46e8364fe3c37bb09
muhammad <css001823@must.ac.mw>
2026-05-04 17:20:54 +0200

    merge changes

--- desk-navigation.php
+++ desk-navigation.php
@@ -1,10 +1,10 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+if (session_status() == PHP_SESSION_NONE)
+    {
+session_start();
+    }
         // Protect page (redirect if not logged in)
-        if (!isset($_SESSION['user_id']) && $_SESSION['role'] != 'desk') {
-        if (!isset($_SESSION['user_id']) && $_SESSION['role'] != 'desk') {
+        if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'desk') {
             header("Location: login.php");
             exit();
         }
